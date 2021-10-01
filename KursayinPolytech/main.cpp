279c91345e2cf8aa09f1307d6f48f643d7a879b7
sergeymkrt <smkrtum@yandex.ru>
2021-10-01 22:02:39 +0400

    Initial Commit

---
+++ KursayinPolytech/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  KursayinPolytech
+//
+//  Created by Sergey Mkrtumyan on 01.10.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
